040ae709782c37d827d7ab8bb298ac7d100de045
Fassio Blatter <fassio@specpose.com>
2023-05-28 12:28:41 +0100

    REFACTORING Requires C++20

--- include/software-on-silicon/TypedWires.hpp
+++ include/software-on-silicon/TypedWires.hpp
@@ -1,5 +1,7 @@
 #include <tuple>
 #include <atomic>
+#include <thread>
+#include <functional>
 
 namespace SOS{
     namespace MemoryView {
@@ -7,6 +9,20 @@ namespace SOS{
             
         };
     }
+    template<typename T> class Thread : private std::jthread {
+        public:
+        Thread(T functor) : functorCopy(functor) {
+            auto thread = std::jthread{std::mem_fn(&T::operator()),functorCopy};
+            this->swap(thread);
+        };
+        ~Thread() {this->join();}
+        virtual void operator()() final{
+            if (this->joinable())
+                this->join();
+        };
+        private:
+        T functorCopy;
+    };
     namespace Behavior {
         template<typename T> class EventHandler {
             public:
--- test/TypedWires.cpp
+++ test/TypedWires.cpp
@@ -4,16 +4,25 @@
 #include <iostream>
 #include <chrono>
 
+class MyFunctor {
+    public:
+    void operator()(){
+        std::cout << message <<std::endl;
+    }
+    private:
+    std::string message = std::string{"Thread is running."};
+};
+
 enum {
-    blink,
-    quit
+    blink
 };
 
 template<typename T>class HandlerImpl : public SOS::Behavior::EventHandler<T> {
     public:
     HandlerImpl(SOS::MemoryView::TypedWires<T>& databus) : SOS::Behavior::EventHandler<T>(databus) {}
     void operator()() final {
-        while(true){
+        const auto start = std::chrono::high_resolution_clock::now();
+        while(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now()-start).count()<10){
             std::get<blink>(this->_wires).store(true);
             std::this_thread::sleep_for(std::chrono::milliseconds{800});
             std::get<blink>(this->_wires).store(false);
@@ -23,7 +32,11 @@ template<typename T>class HandlerImpl : public SOS::Behavior::EventHandler<T> {
 };
 
 int main () {
-    auto myWires = SOS::MemoryView::TypedWires<bool>{false};
+    auto data = MyFunctor();
+    auto t1 = SFA::Thread<MyFunctor>(data);
+    t1();
+
+    /*auto myWires = SOS::MemoryView::TypedWires<bool>{false};
     std::cout<<"Wire initialised to "<<std::get<0>(myWires).load()<<std::endl;
     auto myHandler = HandlerImpl<bool>(myWires);
     std::cout<<"Wire before EventHandler start "<<std::get<blink>(myWires).load()<<std::endl;
@@ -37,5 +50,5 @@ int main () {
             std::cout<<"false... ";
     }
     myThread();
-    std::cout<<"Wire after EventHandler end "<<std::get<blink>(myWires).load()<<std::endl;
+    std::cout<<"Wire after EventHandler end "<<std::get<blink>(myWires).load()<<std::endl;*/
 };
