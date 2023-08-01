fdb367d3d1c27c4826576398047c12df5120a44c
Fassio Blatter <fassio@specpose.com>
2023-08-01 13:06:17 +0100

    FIXED includes

--- impl/MemoryController.cpp
+++ impl/MemoryController.cpp
@@ -1,5 +1,5 @@
-#include "software-on-silicon/EventLoop.hpp"
 #include "software-on-silicon/error.hpp"
+#include "software-on-silicon/EventLoop.hpp"
 #include "software-on-silicon/loop_helpers.hpp"
 #include "software-on-silicon/MemoryController.hpp"
 #include <chrono>
--- impl/RingToMemory.cpp
+++ impl/RingToMemory.cpp
@@ -9,8 +9,8 @@
     ControllerHost<Reader<SigmaDelta<Writer(GPIO)>>>
 */
 
-#include "software-on-silicon/EventLoop.hpp"
 #include "software-on-silicon/error.hpp"
+#include "software-on-silicon/EventLoop.hpp"
 #include "software-on-silicon/loop_helpers.hpp"
 #include "software-on-silicon/RingBuffer.hpp"
 #include "software-on-silicon/MemoryController.hpp"
