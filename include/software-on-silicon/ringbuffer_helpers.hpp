af260fbd992391427aba2d052faa41a2f535a98a
Fassio Blatter <fassio@specpose.com>
2023-08-06 17:47:37 +0100

    UNDO Merge branch 'main' into ARAFallback

--- include/software-on-silicon/ringbuffer_helpers.hpp
+++ include/software-on-silicon/ringbuffer_helpers.hpp
@@ -1,4 +1,8 @@
 namespace SOSFloat {
+template<typename Piece> void PieceWriter_write(typename Piece::iterator current,const SAMPLE_SIZE* buffer[], const std::size_t channels, const std::size_t sampleIndex){
+    for (int channel=0;channel<channels;channel++)
+        (**current)[channel]=buffer[channel][sampleIndex];
+}
 template<typename Piece> void PieceWriter(SOS::MemoryView::RingBufferBus<Piece>& myBus, const SAMPLE_SIZE* buffer[], const std::size_t channels, const std::size_t length, const std::size_t position=0){//value type, amount, offset, (value_detail)
     auto current = std::get<0>(myBus.cables).getCurrentRef().load();
     const auto start = std::get<0>(myBus.const_cables).getWriterStartRef();
@@ -8,8 +12,7 @@ template<typename Piece> void PieceWriter(SOS::MemoryView::RingBufferBus<Piece>&
     for (std::size_t i=0;i<length;i++){//up to length 9
     if (current!=std::get<0>(myBus.cables).getThreadCurrentRef().load()){
         //write directly to HOSTmemory
-        for (int channel=0;channel<channels;channel++)
-            (**current)[channel]=buffer[channel][i];
+        PieceWriter_write<Piece>(current,buffer,channels,i);
         ++current;
         if (current==std::get<0>(myBus.const_cables).getWriterEndRef())
             current = std::get<0>(myBus.const_cables).getWriterStartRef();
@@ -17,8 +20,7 @@ template<typename Piece> void PieceWriter(SOS::MemoryView::RingBufferBus<Piece>&
         myBus.signal.getNotifyRef().clear();
     } else {
         //write last bit
-        for (int channel=0;channel<channels;channel++)
-            (**current)[channel]=buffer[channel][i];
+        PieceWriter_write<Piece>(current,buffer,channels,i);
         //do not advance -> current invalid  
         std::cout<<std::endl;
         throw SFA::util::runtime_error("RingBuffer too slow or not big enough",__FILE__,__func__);
