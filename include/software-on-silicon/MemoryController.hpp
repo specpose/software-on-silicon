21be3dbba96ca21b416ff8b6e72b395cb79a5680
Fassio Blatter <fassio@specpose.com>
2023-08-26 20:49:29 +0100

    FIXED reader bug

--- include/software-on-silicon/MemoryController.hpp
+++ include/software-on-silicon/MemoryController.hpp
@@ -118,17 +118,18 @@ namespace SOS {
                 for (std::size_t channel=0;channel<_size.size();channel++){
                 auto current = _size[channel].getReadBufferStartRef().load();
                 const auto end = _size[channel].getReadBufferAfterLastRef().load();
-                const auto readOffset = _offset.getReadOffsetRef().load();
+                auto readOffset = _offset.getReadOffsetRef().load();
                 if (readOffset<0)
                     throw SFA::util::runtime_error("Negative read offset supplied",__FILE__,__func__);
                 if (std::distance(_memorycontroller_size.getBKStartRef().load(),_memorycontroller_size.getBKEndRef().load())
                 <(std::distance(current,end)+readOffset))
                     throw SFA::util::runtime_error("Read index out of bounds",__FILE__,__func__);
-                auto readerPos = _memorycontroller_size.getBKStartRef().load()+readOffset;
                 while (current!=end){
                     if (!wait()) {
+                        //readOffset stays valid with resize (grow), but not clearMemoryController
+                        auto readerPos = _memorycontroller_size.getBKStartRef().load()+readOffset;
                         *current = (**readerPos)[channel];
-                        readerPos++;
+                        readOffset++;
                         ++current;
                     }
                 }
