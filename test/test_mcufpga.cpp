1e4e01bcdb597a4ce43438062afdf6465ce98b41
Fassio Blatter <fassio@specpose.com>
2024-01-01 12:37:50 +0000

    REFACTORING test_mcufpga line cut

--- test/test_mcufpga.cpp
+++ test/test_mcufpga.cpp
@@ -12,32 +12,37 @@ int main () {
     fpgabus.signal.getAcknowledgeRef().clear();//INIT: start one-way handshake
     auto client= new FPGA(fpgabus);
     const auto start = std::chrono::high_resolution_clock::now();
-    while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start).count() < 1) {
+    while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start).count() < 1) {//CUTS THE LINE => no last sync possible
         //SIMULATION: requires additional thread. => remove thread from parent Controllers
-        if (!mcubus.signal.getAcknowledgeRef().test_and_set()){
+        //if (termios.read(Ctx,&mcu_in_buffer,1)){//SIMULATION: Enable 2
+        //    mcubus.signal.getUpdatedRef().clear();//SIMULATION: Enable 2
+        //    while (mcubus.signal.getAcknowledgeRef().test_and_set())
+        //        std::this_thread::yield();
+        if (!mcubus.signal.getAcknowledgeRef().test_and_set()){//SIMULATION: Disable 2
             //transfer mcu_out_buffer to fpga_in_buffer
             for (std::size_t n=0;n<fpga_in_buffer.size();n++)
                 fpga_in_buffer[n]=mcu_out_buffer[n];
             //SIMULATION: termios write(Ctx,&mcu_out_buffer,1)
             fpgabus.signal.getUpdatedRef().clear();//SIMULATION: Disable 2
         }
-        //if (termios.read(Ctx,&mcu_in_buffer,1)){//SIMULATION: Enable 2
-            //mcubus.signal.getUpdatedRef().clear();//SIMULATION: Enable 2
         //}
-        if (!fpgabus.signal.getAcknowledgeRef().test_and_set()){
+        //if (termios.read(Ctx,&fpga_in_buffer,1)){//SIMULATION: Enable 1
+        //    fpgabus.signal.getUpdatedRef().clear();//SIMULATION: Enable 1
+        //    while (fpgabus.signal.getAcknowledgeRef().test_and_set())
+        //        std::this_thread::yield();
+        if (!fpgabus.signal.getAcknowledgeRef().test_and_set()){//SIMULATION: Disable 1
             //transfer fpga_out_buffer to mcu_in_buffer
             for (std::size_t n=0;n<mcu_in_buffer.size();n++)
                 mcu_in_buffer[n]=fpga_out_buffer[n];
             //SIMULATION: termios write(Ctx,&fpga_out_buffer,1)
             mcubus.signal.getUpdatedRef().clear();//SIMULATION: Disable 1
         }
-        //if (termios.read(Ctx,&fpga_in_buffer,1)){//SIMULATION: Enable 1
-            //fpgabus.signal.getUpdatedRef().clear();//SIMULATION: Enable 1
         //}
         std::this_thread::yield();
     }
     //host._child.stop();
     //host.stop();
     delete client;
+    std::this_thread::sleep_for(std::chrono::milliseconds{500});
     delete host;
 }
