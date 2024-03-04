34769fe14b4a4e365df51c3b63508e40e909b5ad
Fassio Blatter <fassio@specpose.com>
2024-03-04 12:31:55 +0000

    REFACTORING Serial example datatypes

---
+++ impl/MCUFPGA/SymbolRateCounter.cpp
@@ -0,0 +1,30 @@
+struct SymbolRateCounter {
+    SymbolRateCounter(){
+        setNumber(0);
+        set_mcu_owned();
+    }
+    bool mcu_owned(){
+        std::bitset<8> first_byte{static_cast<unsigned int>(StatusAndNumber[0])};
+        first_byte = (first_byte >> 7) << 7;
+        bool result = first_byte[7];//bigend => numbering is right to left, Stroustrup 34.2.2
+        return result;
+    }
+    void set_mcu_owned(bool state=true){
+        std::bitset<8> save_first_byte{static_cast<unsigned int>(StatusAndNumber[0])};
+        save_first_byte[7]= state;
+        StatusAndNumber[0]=static_cast<decltype(StatusAndNumber)::value_type>(save_first_byte.to_ulong());
+    }
+    unsigned int getNumber(){
+        std::bitset<24> combined_bits;
+        bytearrayToBitset(combined_bits,StatusAndNumber);
+        auto stripped = ((combined_bits << 1) >> 1);
+        return stripped.to_ulong();
+    }
+    void setNumber(unsigned int number){
+        bool save_ownership = mcu_owned();
+        std::bitset<24> combined_bits{number};
+        combined_bits[23]=save_ownership;//mcu_owned        
+        bitsetToBytearray(StatusAndNumber,combined_bits);
+    }
+    std::array<unsigned char,3> StatusAndNumber;//23 bits number: unsigned maxInt 8388607
+};
