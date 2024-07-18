f86c0668aa4d964fccbff0ff64259715888523bf
Fassio Blatter <fassio@specpose.com>
2024-07-18 21:02:32 +0200

    REFACTORING Serial simulation threaded com

--- test/test_mcufpga.cpp
+++ test/test_mcufpga.cpp
@@ -4,48 +4,61 @@ COM_BUFFER fpga_out_buffer;
 COM_BUFFER mcu_in_buffer;
 COM_BUFFER mcu_out_buffer;
 
+void client_funct(COM_BUFFER& fpga_in_buffer, COM_BUFFER& mcu_in_buffer, COM_BUFFER& fpga_out_buffer, COM_BUFFER& mcu_out_buffer,
+    SOS::MemoryView::ComBus<COM_BUFFER>& mcubus,SOS::MemoryView::ComBus<COM_BUFFER>& fpgabus){
+    if (!fpgabus.signal.getAcknowledgeRef().test_and_set()){
+        //transfer fpga_out_buffer to mcu_in_buffer
+        for (std::size_t n=0;n<mcu_in_buffer.size();n++)
+            mcu_in_buffer[n]=fpga_out_buffer[n];
+        mcubus.signal.getUpdatedRef().clear();
+    }
+    std::this_thread::yield();
+};
+void host_funct(COM_BUFFER& fpga_in_buffer, COM_BUFFER& mcu_in_buffer, COM_BUFFER& fpga_out_buffer, COM_BUFFER& mcu_out_buffer,
+    SOS::MemoryView::ComBus<COM_BUFFER>& mcubus,SOS::MemoryView::ComBus<COM_BUFFER>& fpgabus){
+    if (!mcubus.signal.getAcknowledgeRef().test_and_set()){
+        //transfer mcu_out_buffer to fpga_in_buffer
+        for (std::size_t n=0;n<fpga_in_buffer.size();n++)
+            fpga_in_buffer[n]=mcu_out_buffer[n];
+        fpgabus.signal.getUpdatedRef().clear();
+    }
+    std::this_thread::yield();
+};
+
 int main () {
     SOS::MemoryView::ComBus<COM_BUFFER> mcubus{std::begin(mcu_in_buffer),std::end(mcu_in_buffer),std::begin(mcu_out_buffer),std::end(mcu_out_buffer)};
     auto host= new MCU(mcubus);//SIMULATION: requires additional thread. => remove thread from MCU
+    bool host_request_stop = false;
     fpga_out_buffer[0]=SOS::Protocol::idleState().to_ulong();//INIT: FPGA initiates communication with an idle byte
     SOS::MemoryView::ComBus<COM_BUFFER> fpgabus{std::begin(fpga_in_buffer),std::end(fpga_in_buffer),std::begin(fpga_out_buffer),std::end(fpga_out_buffer)};
     fpgabus.signal.getAcknowledgeRef().clear();//INIT: start one-way handshake
     auto client= new FPGA(fpgabus);//SIMULATION: requires additional thread. => remove thread from FPGA
-    const auto start = std::chrono::high_resolution_clock::now();
     bool client_request_stop = false;
-    bool host_stop = false;
     bool stop = false;
+    const auto start = std::chrono::high_resolution_clock::now();
     while (!stop) {
+        if (host->isStopped() && client->isStopped())
+            stop = true;
+
         //HOST THREAD
-        if (!mcubus.signal.getAcknowledgeRef().test_and_set()){
-            //transfer mcu_out_buffer to fpga_in_buffer
-            for (std::size_t n=0;n<fpga_in_buffer.size();n++)
-                fpga_in_buffer[n]=mcu_out_buffer[n];
-            fpgabus.signal.getUpdatedRef().clear();
-        }
-
-        //SIMULATION ONLY
-	if (host_stop){
-	    stop = true;//CUTS THE LINE
-	}
-
         if (!(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start).count() < 1)){
-            if (!client_request_stop){
-	        client->requestStop();
-                client_request_stop = true;
-	    } else {
-	    if (client->isStopped()){//client sync finished
-		    host_stop = true;
-        }}
-	}
+            if (!host_request_stop){
+                host->requestStop();
+                host_request_stop = true;
+            }
+        }
+        if (!host->isStopped())
+            host_funct(fpga_in_buffer, mcu_in_buffer, fpga_out_buffer, mcu_out_buffer, mcubus, fpgabus);
 
         //CLIENT THREAD
-        if (!fpgabus.signal.getAcknowledgeRef().test_and_set()){
-            //transfer fpga_out_buffer to mcu_in_buffer
-            for (std::size_t n=0;n<mcu_in_buffer.size();n++)
-                mcu_in_buffer[n]=fpga_out_buffer[n];
-            mcubus.signal.getUpdatedRef().clear();
+        if (!(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start).count() < 1)){
+            if (!client_request_stop){
+	            client->requestStop();
+                client_request_stop = true;
+	        }
         }
+        if (!client->isStopped())
+            client_funct(fpga_in_buffer, mcu_in_buffer, fpga_out_buffer, mcu_out_buffer, mcubus, fpgabus);
 
         std::this_thread::yield();
     }
