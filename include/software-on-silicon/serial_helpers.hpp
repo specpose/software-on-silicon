292be79394e87589d040d943dfd6000bfea26175
Fassio Blatter <fassio@specpose.com>
2024-09-17 19:18:28 +0200

    FIXED2 gcc-13 compile

--- include/software-on-silicon/serial_helpers.hpp
+++ include/software-on-silicon/serial_helpers.hpp
@@ -1,26 +1,26 @@
 #include <bitset>
 #include <cmath>
-template<unsigned long Bytes> constexpr unsigned long long maxUnsignedInt() {
+template<size_t Bytes> constexpr unsigned long long maxUnsignedInt() {
     unsigned long long result = 0;
     for (int i=0;i<Bytes;i++){
         result+= 0xFF*std::pow(256,i);
     }
     return result;
 };
-template<unsigned long Bytes> constexpr unsigned long long bytearrayToUnsignedNumber(std::array<unsigned char,Bytes>& source){
+template<size_t Bytes> constexpr unsigned long long bytearrayToUnsignedNumber(std::array<unsigned char,Bytes>& source){
     unsigned long long result = 0;
     for (int i=0;i<Bytes;i++){
         result+= static_cast<unsigned char>(source[i])*std::pow(256,i);
     }
     return result;
 };
-template<unsigned long Bytes, unsigned long Bits> constexpr void bitsetToBytearray(std::array<unsigned char,Bytes>& dest,std::bitset<Bits>& source){
+template<size_t Bytes, size_t Bits> constexpr void bitsetToBytearray(std::array<unsigned char,Bytes>& dest,std::bitset<Bits>& source){
     if (source.to_ullong() > maxUnsignedInt<Bytes>())
         throw SFA::util::logic_error("BitSet does not fit into char[]",__FILE__,__func__);
     for (int i=0;i<Bytes;i++)//bitset is little endian, first destination byte is bigend
         dest[i]=static_cast<unsigned char>(((source << (Bytes-(Bytes-i))*8) >> (Bytes-1)*8).to_ulong());;
 };
-template<unsigned long Bytes, unsigned long Bits> constexpr void bytearrayToBitset(std::bitset<Bits>& dest,std::array<unsigned char,Bytes>& source){
+template<size_t Bytes, size_t Bits> constexpr void bytearrayToBitset(std::bitset<Bits>& dest,std::array<unsigned char,Bytes>& source){
     std::bitset<Bits> allSet;
     allSet.set();
     if (maxUnsignedInt<Bytes>() > allSet.to_ullong())
