634deb779714b2b1ebf9176102ae0b3651312ca2
Fassio Blatter <fassio@specpose.com>
2024-09-22 00:08:27 +0200

    FIXED5 Serial poweronstate

--- include/software-on-silicon/Serial.hpp
+++ include/software-on-silicon/Serial.hpp
@@ -156,16 +156,20 @@ namespace SOS
                 {
                     if (handshake())
                     {
+                        if (first_run){
+                            poweron_hook();
+                        } else {
                         if (com_shutdown && sent_com_shutdown)
                             finished_com_shutdown = true;
                         if (!receive_lock)
-                            read_hook(read4minus1);
+                            read_hook();
                         else
                             read_object(read4minus1);
                         if (!send_lock)
-                            write_hook(write3plus1);
+                            write_hook();
                         if (send_lock)
                             write_object(write3plus1);
+                        }
                         handshake_ack();
                     }
                     if (loop_shutdown && finished_com_shutdown)
@@ -229,7 +233,7 @@ namespace SOS
             bool finished_com_shutdown = false;
             bool receive_lock = false;
             bool send_lock = false;
-            void read_hook(int &read4minus1)
+            void read_hook()
             {
                 unsigned char data = read_byte();
                 read_bits(static_cast<unsigned long>(data));
@@ -291,19 +295,17 @@ namespace SOS
                     //}
                 }
             }
-            void write_hook(int &write3plus1)
+            void poweron_hook(){
+                auto id = poweronState();
+                write_bits(id);
+                id.set(7, 1); // override write_bits
+                std::cout<<typeid(*this).name();
+                std::cout<<"P";
+                write_byte(static_cast<unsigned char>(id.to_ulong()));
+                first_run=false;
+            }
+            void write_hook()
             {
-                if (first_run){
-                    if (receive_acknowledge())
-                        throw SFA::util::logic_error("Received a transfer acknowledge during poweron.", __FILE__, __func__);
-                    auto id = poweronState();
-                    write_bits(id);
-                    id.set(7, 1); // override write_bits
-                    std::cout<<typeid(*this).name();
-                    std::cout<<"P";
-                    write_byte(static_cast<unsigned char>(id.to_ulong()));
-                    first_run=false;
-                } else {
                 if (receive_acknowledge())
                 {
                     send_lock = true;
@@ -332,23 +334,11 @@ namespace SOS
                         }
                     }
                 }
-                }
             }
             unsigned int writeCount = 0; // write3plus1
             std::size_t writeOriginPos = 0;
             unsigned int readCount = 0; // read4minus1
             std::size_t readDestinationPos = 0;
-            void _write_transfer_request(unsigned char objectid){
-                send_request();
-                foreign().writeOrigin().store(objectid);
-                writeOriginPos = 0;
-                std::bitset<8> id;
-                write_bits(id);
-                std::bitset<8> obj_id = static_cast<unsigned long>(foreign().writeOrigin().load()); // DANGER: overflow check
-                id = id ^ obj_id;
-                // std::cout<<typeid(*this).name()<<" sending WriteOrigin "<<foreign().writeOrigin()<<std::endl;
-                write_byte(static_cast<unsigned char>(id.to_ulong()));
-            }
             bool getFirstSyncObject()
             {
                 bool gotOne = false;
@@ -359,7 +349,15 @@ namespace SOS
                         throw SFA::util::logic_error("DMAObject has entered an illegal sync state.", __FILE__, __func__);
                     if (!foreign().descriptors[i].synced)
                     {
-                        _write_transfer_request(foreign().descriptors[i].id);
+                        send_request();
+                        foreign().writeOrigin().store(foreign().descriptors[i].id);
+                        writeOriginPos = 0;
+                        std::bitset<8> id;
+                        write_bits(id);
+                        std::bitset<8> obj_id = static_cast<unsigned long>(foreign().writeOrigin().load()); // DANGER: overflow check
+                        id = id ^ obj_id;
+                        // std::cout<<typeid(*this).name()<<" sending WriteOrigin "<<foreign().writeOrigin()<<std::endl;
+                        write_byte(static_cast<unsigned char>(id.to_ulong()));
                         gotOne = true;
                     }
                 }
