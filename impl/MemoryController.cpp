d199426927802d06e814080d8be57a29c2f16d34
Fassio Blatter <fassio@specpose.com>
2025-12-29 09:21:59 +0100

    FIXED error_codes are in impl

--- impl/MemoryController.cpp
+++ impl/MemoryController.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "software-on-silicon/error.hpp"
+#include "error.cpp"
 #include "software-on-silicon/INTERFACE.hpp"
 #include "software-on-silicon/memorycontroller_helpers.hpp"
 #include "software-on-silicon/rtos_helpers.hpp"
