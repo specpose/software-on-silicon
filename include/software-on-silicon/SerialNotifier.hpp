6c70b3a30c035f85db242407b8b6a121b6e34a18
Fassio Blatter <fassio@specpose.com>
2026-02-17 13:21:44 +0100

    ADDED apply for Cpp14

--- include/software-on-silicon/SerialNotifier.hpp
+++ include/software-on-silicon/SerialNotifier.hpp
@@ -159,13 +159,36 @@ namespace Protocol {
         std::size_t count = 0;
     };
 }
+//https://en.cppreference.com/w/cpp/utility/integer_sequence.html
+namespace detail {
+    template<class T, T I, T N, T... integers>
+    struct make_integer_sequence_helper
+    {
+        using type = typename make_integer_sequence_helper<T, I + 1, N, integers..., I>::type;
+    };
+
+    template<class T, T N, T... integers>
+    struct make_integer_sequence_helper<T, N, N, integers...>
+    {
+        using type = std::integer_sequence<T, integers...>;
+    };
+}
+template<class T, T N> using make_integer_sequence = typename detail::make_integer_sequence_helper<T, 0, N>::type;
+//https://stackoverflow.com/questions/7858817/unpacking-a-tuple-to-call-a-matching-function-pointer
+template<typename Function, typename ArgsTuple, std::size_t... I> auto apply(Function& func, ArgsTuple&& args, std::integer_sequence<std::size_t,I...>) {
+    return func(std::get<I>(args)...);
+}
+template<typename Function, typename ArgsTuple> auto apply(Function& func, ArgsTuple& args) {
+    auto _args = std::forward_as_tuple(args);
+    return apply(func, args, make_integer_sequence<std::size_t, std::tuple_size<ArgsTuple>::value>{} );
+}
 namespace MemoryView {
 
     template <typename... Objects>
     struct SerialProcessNotifier : public BusDMAShaker {
         SerialProcessNotifier()
         {
-            std::apply(descriptors, objects); // ALWAYS: Initialize Descriptors in Constructor
+            apply(descriptors, objects); // ALWAYS: Initialize Descriptors in Constructor
         }
         std::tuple<Objects...> objects {};
         SOS::Protocol::DescriptorHelper<std::tuple_size<std::tuple<Objects...>>::value> descriptors {};
