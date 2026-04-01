561b685582493762ed14874fd3753d0ee1561954
Fassio Blatter <fassio@specpose.com>
2026-04-01 15:19:56 +0200

    REFACTORING4 ringbuffer_helpers

--- include/software-on-silicon/ringbuffer_helpers.hpp
+++ include/software-on-silicon/ringbuffer_helpers.hpp
@@ -6,14 +6,13 @@ template<typename Current, typename Buffer> void write_blink_interleaved(Current
     for (std::size_t sample=0;sample<std::tuple_size<RING_BUFFER::value_type>{};sample++)
         current[sample]=buffer[sample];
 }
-template<typename Target, typename... Args> void WriteInterleaved(SOS::MemoryView::RingBufferBus<Target>& myBus, Args...) {};
-template<typename Target, typename InputBlock> void WriteInterleaved(SOS::MemoryView::RingBufferBus<Target>& myBus, const InputBlock& buffer){
+template<typename Target, typename... Args> void WriteInterleaved(SOS::MemoryView::RingBufferBus<Target>& myBus, Args... args) {
     auto current = std::get<0>(myBus.cables).getCurrentRef().load();
     const auto start = std::get<0>(myBus.const_cables).getWriterStartRef();
     const auto end = std::get<0>(myBus.const_cables).getWriterEndRef();
     if (current!=std::get<0>(myBus.cables).getThreadCurrentRef().load()){
         //write directly to HOSTmemory
-        write_blink_interleaved(*current, buffer);
+        write_blink_interleaved(*current, args...);
         ++current;
         if (current==std::get<0>(myBus.const_cables).getWriterEndRef())
             current = std::get<0>(myBus.const_cables).getWriterStartRef();
@@ -21,7 +20,7 @@ template<typename Target, typename InputBlock> void WriteInterleaved(SOS::Memory
         myBus.signal.getNotifyRef().clear();
     } else {
         //write last bit
-        write_blink_interleaved(*current, buffer);
+        write_blink_interleaved(*current, args...);
         //current invalid => do not advance
         std::cout<<std::endl;
         SFA::util::runtime_error(SFA::util::error_code::RingbufferTooSlowOrNotBigEnough,__FILE__,__func__);
