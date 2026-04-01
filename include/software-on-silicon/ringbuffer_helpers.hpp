fcc4ab7c00ab15961c4e600ffbe1a3c17bcc7ccb
Fassio Blatter <fassio@specpose.com>
2026-04-01 12:03:17 +0200

    REFACTORING3 ringbuffer_helpers

--- include/software-on-silicon/ringbuffer_helpers.hpp
+++ include/software-on-silicon/ringbuffer_helpers.hpp
@@ -1,10 +1,10 @@
 template<typename Current, typename... Args> void write_blink_interleaved(Current current, Args... args) {
     std::cout<<"=";
 };
-template<typename Current, typename Buffer> void write_blink_interleaved(Current current, const Buffer& buffer){
+template<typename Current, typename Buffer> void write_blink_interleaved(Current& current, const Buffer& buffer){
     std::cout<<"=";
     for (std::size_t sample=0;sample<std::tuple_size<RING_BUFFER::value_type>{};sample++)
-        (*current)[sample]=buffer[sample];
+        current[sample]=buffer[sample];
 }
 template<typename Target, typename... Args> void WriteInterleaved(SOS::MemoryView::RingBufferBus<Target>& myBus, Args...) {};
 template<typename Target, typename InputBlock> void WriteInterleaved(SOS::MemoryView::RingBufferBus<Target>& myBus, const InputBlock& buffer){
@@ -13,7 +13,7 @@ template<typename Target, typename InputBlock> void WriteInterleaved(SOS::Memory
     const auto end = std::get<0>(myBus.const_cables).getWriterEndRef();
     if (current!=std::get<0>(myBus.cables).getThreadCurrentRef().load()){
         //write directly to HOSTmemory
-        write_blink_interleaved(current, buffer);
+        write_blink_interleaved(*current, buffer);
         ++current;
         if (current==std::get<0>(myBus.const_cables).getWriterEndRef())
             current = std::get<0>(myBus.const_cables).getWriterStartRef();
@@ -21,7 +21,7 @@ template<typename Target, typename InputBlock> void WriteInterleaved(SOS::Memory
         myBus.signal.getNotifyRef().clear();
     } else {
         //write last bit
-        write_blink_interleaved(current, buffer);
+        write_blink_interleaved(*current, buffer);
         //current invalid => do not advance
         std::cout<<std::endl;
         SFA::util::runtime_error(SFA::util::error_code::RingbufferTooSlowOrNotBigEnough,__FILE__,__func__);
