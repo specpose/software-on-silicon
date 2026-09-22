6bfb095610175d58cc19e3c88d301aa5a3dcd9f7
Fassio Blatter <fassio@specpose.com>
2026-09-22 14:34:32 +0200

    FIXED Notify Processor Scheme

--- impl/Controller.cpp
+++ impl/Controller.cpp
@@ -6,16 +6,15 @@
 using namespace SOS::MemoryView;
 using namespace std::chrono;
 
-// needs a bus because it is a subcontroller
-class SubControllerImpl : public SOS::Behavior::SimpleDummy {
+class NotifyImpl : public SOS::Behavior::SimpleDummy {
 public:
     using bus_type = SOS::MemoryView::BusNotifier;
-    SubControllerImpl(bus_type& bus)
+    NotifyImpl(bus_type& bus)
         : SOS::Behavior::SimpleDummy(bus.signal)
     {
         _thread = start(this);
     }
-    ~SubControllerImpl() final
+    ~NotifyImpl() final
     {
         destroy(_thread);
     }
@@ -36,36 +35,58 @@ private:
     std::thread _thread;
 };
 
-// A RunLoop is not a Loop, because it does not have a signal
-class ControllerImpl : public SOS::Behavior::EventController<SubControllerImpl> {
+class ProcessorScheme : public SOS::Behavior::SimpleDummy {
 public:
-    ControllerImpl(bus_type& bus)
-        : EventController<SubControllerImpl>(bus.signal)
-        , waiter(new SystemTimer<milliseconds, MEASUREMENT_UNIT_IN_MILLIS>(waiterBus.signal))
+    using bus_type = SOS::MemoryView::BusNotifier;
+    ProcessorScheme(bus_type& bus)
+    : SOS::Behavior::SimpleDummy(bus.signal)
     {
+        _intrinsic.getNotifyRef().clear();
         _thread = start(this);
     }
-    ~ControllerImpl()
+    ~ProcessorScheme() final
     {
         destroy(_thread);
-        std::cout << "Controller loop has terminated." << std::endl;
-        delete waiter;
     }
     void event_loop()
     {
-        if (!_intrinsic.getUpdatedRef().test_and_set()) {
-            waiterBus.signal.getUpdatedRef().clear();
-            if (!waiterBus.signal.getAcknowledgeRef().test_and_set()) {
-                if (!_foreign.signal.getNotifyRef().test_and_set()) {
-                    _foreign.signal.getNotifyRef().clear();
-                    std::cout << "*";
-                } else {
-                    std::cout << "_";
-                }
-            }
-            _intrinsic.getAcknowledgeRef().clear();
+        for (int i = 0; i < 3; ++i) {
+        // would: acquire new data through a wire
+        // blink on
+        while (_intrinsic.getNotifyRef().test_and_set())
+            std::this_thread::yield();
+        // run
+        std::cout << "*";
+        // blink off
+        _intrinsic.getNotifyRef().clear();
+        std::this_thread::sleep_for(milliseconds { 111 });
         }
+        // pause
+        for (int i = 0; i < 6; ++i) {
+        std::this_thread::sleep_for(milliseconds { 111 });
+        }
+    };
+
+private:
+    std::thread _thread;
+};
+
+template <typename S>
+class ControllerImpl : public SOS::Behavior::EventController<S> {
+public:
+    ControllerImpl(typename SOS::Behavior::EventController<S>::bus_type& bus)
+    : SOS::Behavior::EventController<S>(bus.signal)
+    , waiter(new SystemTimer<milliseconds, MEASUREMENT_UNIT_IN_MILLIS>(waiterBus.signal))
+    {
+        _thread = this->start(this);
     }
+    ~ControllerImpl()
+    {
+        this->destroy(_thread);
+        std::cout << "Controller loop has terminated." << std::endl;
+        delete waiter;
+    }
+    void event_loop();
 
 private:
     bool stop = false;
@@ -73,3 +94,36 @@ private:
     SystemTimer<milliseconds, MEASUREMENT_UNIT_IN_MILLIS>* waiter;
     std::thread _thread;
 };
+
+template <> void ControllerImpl<NotifyImpl>::event_loop()
+{
+    if (!_intrinsic.getUpdatedRef().test_and_set()) {
+        waiterBus.signal.getUpdatedRef().clear();
+        if (!waiterBus.signal.getAcknowledgeRef().test_and_set()) {
+            if (!_foreign.signal.getNotifyRef().test_and_set()) {
+                _foreign.signal.getNotifyRef().clear();
+                std::cout << "*";
+            } else {
+                std::cout << "_";
+            }
+        }
+        _intrinsic.getAcknowledgeRef().clear();
+    }
+}
+
+template <> void ControllerImpl<ProcessorScheme>::event_loop()
+{
+    if (!_intrinsic.getUpdatedRef().test_and_set()) {
+        waiterBus.signal.getUpdatedRef().clear();
+        if (!waiterBus.signal.getAcknowledgeRef().test_and_set()) {
+            for (int i = 0; i < 3; ++i) {
+            while (_foreign.signal.getNotifyRef().test_and_set())
+                std::this_thread::yield();
+            std::cout << "_";
+            _foreign.signal.getNotifyRef().clear();
+            std::this_thread::sleep_for(milliseconds { 111 });
+            }
+        }
+        _intrinsic.getAcknowledgeRef().clear();
+    }
+}
--- test/test_controller.cpp
+++ test/test_controller.cpp
@@ -1,10 +1,15 @@
 #include "Controller.cpp"
 
-int main()
+int main(int argc, char* argv[])
 {
     SOS::MemoryView::BusShaker bus {};
     std::cout << "Controller loop running for 10s..." << std::endl;
-    ControllerImpl* myController = new ControllerImpl(bus);
+    void* myController = nullptr;
+    if (true)
+        myController = (ControllerImpl<ProcessorScheme>*)new ControllerImpl<ProcessorScheme>(bus);
+    else
+         myController = (ControllerImpl<NotifyImpl>*)new ControllerImpl<NotifyImpl>(bus);
+
     const auto start = high_resolution_clock::now();
     std::cout << "main() loop running for 5s..." << std::endl;
     bus.signal.getUpdatedRef().clear();
@@ -14,5 +19,8 @@ int main()
     }
     std::cout << std::endl << "main() loop has terminated." << std::endl;
     std::this_thread::sleep_for(seconds { 5 });
-    delete myController;
+    if (true)
+        delete (ControllerImpl<ProcessorScheme>*)myController;
+    else
+        delete (ControllerImpl<NotifyImpl>*)myController;
 }
